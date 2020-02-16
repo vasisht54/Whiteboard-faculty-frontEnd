b5b1bd7e0b1854fa9fa37235ed52c12cb25a9715
Sanju Vasisht <vasisht.theviper@gmail.com>
2020-02-16 01:41:28 -0500

    Added the boilerplate blueprint for functions in CourseServices.js

---
+++ src/services/CourseService.js
@@ -0,0 +1,20 @@
+export const createCourse = (course) => {
+
+};
+
+export const findAllCourses = () => {
+
+};
+
+export const findCourseById = (id) => {
+
+};
+
+export const updateCourse = (id, course) => {
+
+};
+
+export const deleteCourse = () => {
+
+};
+
