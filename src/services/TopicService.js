00c6061d00acf56509b6e43127897bac2950ad29
vasisht54 <vasisht.theviper@gmail.com>
2020-03-18 15:33:16 -0400

    Implemented Topic Service

---
+++ src/services/TopicService.js
@@ -0,0 +1,41 @@
+import {API_URL} from "../common/constants";
+
+export const updateTopic = async (topicId, topic) => {
+    const response = await fetch(`${API_URL}/topics/${topicId}`, {
+        method: 'PUT',
+        body: JSON.stringify(topic),
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
+    return await response.json()
+};
+
+export const deleteTopic = async (topicId) => {
+    const response = await fetch(`${API_URL}/topics/${topicId}`, {
+        method: 'DELETE'
+    });
+    return await response.json()
+};
+
+export const createTopic = async (lessonId, topic) =>
+{
+    const response = await fetch(`${API_URL}/lessons/${lessonId}/topics`, {
+        method: "POST",
+        body: JSON.stringify(topic),
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
+    return await response.json()
+};
+
+export const findTopicsForLesson = async(lessonId) => {
+    const response = await fetch(`${API_URL}/lessons/${lessonId}/topics`);
+    return await response.json();
+};
+
+export const findTopic = async(topicId) => {
+    const response = await fetch(`${API_URL}/topics/${topicId}`);
+    return await response.json();
+};
